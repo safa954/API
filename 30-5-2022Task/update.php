329f25be4817601e7725467dc0357e90cdb22eeb
safa954 <100359594+safa954@users.noreply.github.com>
2022-05-30 14:23:23 -0700

    main

---
+++ 30-5-2022Task/update.php
@@ -0,0 +1,20 @@
+<?php
+$ServerName = 'localhost';
+$UserName = 'root';
+$Password = '';
+$dbName = 'testapi';
+$dsn = "mysql:host=$ServerName;dbname=$dbName";
+$pdo = new PDO($dsn,$UserName,$Password);
+$id= $_POST['id'];
+$age= $_POST['age'];
+    try {
+        $sql="UPDATE `test1` SET `age` = $age  WHERE ID = $id";
+        $stat=$pdo->query($sql);
+        $resultData = array('status' => true, 'message' => 'Post Update Successfully...');
+    }
+    catch (PDOException $e){
+        $resultData = array('status' => false, 'message' => 'Can\'t able to Update  a post details...');
+    }
+    echo json_encode($resultData);
+
+?>
